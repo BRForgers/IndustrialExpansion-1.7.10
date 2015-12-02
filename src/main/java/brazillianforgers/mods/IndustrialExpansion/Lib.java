4f31a5c92a2aaaad2ff8c171a4e5c3354ed9ff13
allanf181 <allanfarmelin2001@yahoo.com.br>
2015-12-02 18:00:17 -0200

    v1.0b

--- src/main/java/brazillianforgers/mods/IndustrialExpansion/Lib.java
+++ src/main/java/brazillianforgers/mods/IndustrialExpansion/Lib.java
@@ -3,7 +3,7 @@ package brazillianforgers.mods.IndustrialExpansion;
 public class Lib {
     //Mod Identity
     public static final String MODID = "IndustrialExpansion";
-    public static final String VERSION = "1.0a";
+    public static final String VERSION = "1.0b";
     public static final String MODNAME = "Industrial Expansion";
     public static final String DEPS = "required-after:BRForgersCore@[1.2,);required-after:ThermalExpansion";// + ";required-after:MODID"; for Each other MOD
 
