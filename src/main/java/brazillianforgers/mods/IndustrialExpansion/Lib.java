b0e77a8d7afeaa3f135bc023d5c0a96e418defc9
allanf181 <allanfarmelin2001@yahoo.com.br>
2015-12-14 10:48:21 -0200

    drone.io deploy test

--- src/main/java/brazillianforgers/mods/IndustrialExpansion/Lib.java
+++ src/main/java/brazillianforgers/mods/IndustrialExpansion/Lib.java
@@ -3,7 +3,7 @@ package brazillianforgers.mods.IndustrialExpansion;
 public class Lib {
     //Mod Identity
     public static final String MODID = "IndustrialExpansion";
-    public static final String VERSION = "1.0d";
+    public static final String VERSION = "BETA-v1.0";
     public static final String MODNAME = "Industrial Expansion";
     public static final String DEPS = "required-after:BRForgersCore@[1.2,);required-after:ThermalExpansion";// + ";required-after:MODID"; for Each other MOD
 
