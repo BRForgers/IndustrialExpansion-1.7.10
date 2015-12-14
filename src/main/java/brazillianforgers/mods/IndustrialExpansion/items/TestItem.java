c895b773981b27b355fbb97aee8903b9d34122f5
allanf181 <allanfarmelin2001@yahoo.com.br>
2015-12-14 17:43:11 -0200

    TestItem is OverPower

--- src/main/java/brazillianforgers/mods/IndustrialExpansion/items/TestItem.java
+++ src/main/java/brazillianforgers/mods/IndustrialExpansion/items/TestItem.java
@@ -1,19 +1,81 @@
 package brazillianforgers.mods.IndustrialExpansion.items;
 
+import java.util.Set;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
 import brazillianforgers.mods.IndustrialExpansion.IndustrialExpansion;
 import brazillianforgers.mods.IndustrialExpansion.Lib;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import cpw.mods.fml.common.eventhandler.Event.Result;
 
-public class TestItem extends Item
+public class TestItem extends ItemPickaxe
 {
-    public TestItem()
+    @SuppressWarnings("rawtypes")
+    private static Set effectiveAgainst = Sets.newHashSet(new Block[] {Blocks.grass, Blocks.dirt, Blocks.sand, Blocks.gravel, Blocks.snow_layer, Blocks.snow, Blocks.clay, Blocks.farmland, Blocks.soul_sand, Blocks.mycelium});
+    protected TestItem(String unlocalizedName, ToolMaterial material)
     {
+        super (material);
         setUnlocalizedName("Test");
         setTextureName(Lib.MODID + ":" + "TestItem");
         setCreativeTab(IndustrialExpansion.tabIndustrialExpansion);
+
+    }
+    @Override
+    public boolean func_150897_b(Block block) {
+        return effectiveAgainst.contains(block) ? true : super.func_150897_b(block);
+    }
+
+    @Override
+    public float func_150893_a(ItemStack stack, Block block) {
+        return effectiveAgainst.contains(block) ? this.efficiencyOnProperMaterial : super.func_150893_a(stack, block);
+    }
+
+    @Override
+    public Set<String> getToolClasses(ItemStack stack) {
+        return ImmutableSet.of("pickaxe", "spade");
+    }
+
+    @Override
+    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+        if (!player.canPlayerEdit(x, y, z, side, stack)) {
+            return false;
+        } else {
+            UseHoeEvent event = new UseHoeEvent(player, stack, world, x, y, z);
+            if (MinecraftForge.EVENT_BUS.post(event)) {
+                return false;
+            }
+
+            if (event.getResult() == Result.ALLOW) {
+                stack.damageItem(1, player);
+                return true;
+            }
+
+            Block block = world.getBlock(x, y, z);
+
+            if (side != 0 && world.getBlock(x, y + 1, z).isAir(world, x, y + 1, z) && (block == Blocks.grass || block == Blocks.dirt)) {
+                Block block1 = Blocks.farmland;
+                world.playSoundEffect(x + 0.5F, y + 0.5F, z + 0.5F, block1.stepSound.getStepResourcePath(), (block1.stepSound.getVolume() + 1.0F) / 2.0F, block1.stepSound.getPitch() * 0.8F);
+
+                if (world.isRemote) {
+                    return true;
+                }
+                else {
+                    world.setBlock(x, y, z, block1);
+                    stack.damageItem(1, player);
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
     }
 
 }
