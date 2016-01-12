33594d94ce475844bcb761a5c70fe27447327788
allanf181 <allanfarmelin2001@yahoo.com.br>
2016-01-11 22:50:24 -0200

    ..

--- src/main/java/brazillianforgers/mods/IndustrialExpansion/manager/RecipeManager.java
+++ src/main/java/brazillianforgers/mods/IndustrialExpansion/manager/RecipeManager.java
@@ -1,20 +1,22 @@
 package brazillianforgers.mods.IndustrialExpansion.manager;
 
+import brazillianforgers.mods.IndustrialExpansion.IndustrialExpansion;
 import brazillianforgers.mods.IndustrialExpansion.helper.ConfigHandler;
-import brazillianforgers.mods.IndustrialExpansion.items.*;
+import brazillianforgers.mods.IndustrialExpansion.manager.ItemManager;
 import cpw.mods.fml.common.event.FMLInterModComms;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import brazillianforgers.mods.IndustrialExpansion.helper.dustCoalHelper;
 
 public class RecipeManager {
     public static void init() {
         //Base: addInductionSmelterRecipe(CustoDeEnergia, Input1, Input2, Output1);
         //Um Input/Output seria algo tipo: new ItemStack(ItemManager.NomeDoItem)
-        addSmelterRecipe(ConfigHandler.energyCoalBall, new ItemStack(Items.coal), new ItemStack(Items.flint), MetaItemGetter.conductiveIron);
-        addSmelterRecipe(ConfigHandler.energyPhasedIron, new ItemStack(Items.iron_ingot), new ItemStack(Items.ender_pearl), MetaItemGetter.phasedIron);
+        addSmelterRecipe(ConfigHandler.energyCoalBall, new ItemStack(dustCoalHelper.dustCoal), new ItemStack(Items.flint), ItemManager.CoalBall);
+        addSmelterRecipe(ConfigHandler.energyCoalChunk, new ItemStack(), new ItemStack(Blocks.obsidian), MetaItemGetter.phasedIron);
     }
 
     public static void addInductionSmelterRecipe(int energyCost, ItemStack primaryInput, ItemStack secondaryInput, ItemStack primaryOutput)
