bdffe061d10c8e27e987c869e232cde65fa41c76
allanf181 <allanfarmelin2001@yahoo.com.br>
2016-01-11 23:34:37 -0200

    Tentando dnv.

--- src/main/java/brazillianforgers/mods/IndustrialExpansion/manager/SmelterRecipeManager.java
+++ src/main/java/brazillianforgers/mods/IndustrialExpansion/manager/SmelterRecipeManager.java
@@ -10,8 +10,8 @@ import net.minecraft.item.crafting.FurnaceRecipes;
 public class SmelterRecipeManager {
     public static void init(){
         // Note not only Block -> Blocks but stoneBrick -> stonebrick
-        GameRegistry.addSmelting(ItemManager.CoalBall, new ItemStack(ItemManager.BurnedCoalBall), 0.1f);
-        GameRegistry.addSmelting(ItemManager.CoalChunk, new ItemStack(Items.diamond), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(ItemManager.CoalBall), new ItemStack(ItemManager.BurnedCoalBall), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(ItemManager.CoalChunk), new ItemStack(Items.diamond), 0.1f);
     }
 
 }
