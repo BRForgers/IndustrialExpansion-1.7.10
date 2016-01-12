952a31a8bb19f56ff40cb5600e2a7a9e4e7416ca
allanf181 <allanfarmelin2001@yahoo.com.br>
2016-01-11 22:18:35 -0200

    Update RecipeManager.java

--- src/main/java/brazillianforgers/mods/IndustrialExpansion/manager/RecipeManager.java
+++ src/main/java/brazillianforgers/mods/IndustrialExpansion/manager/RecipeManager.java
@@ -1,7 +1,34 @@
 package brazillianforgers.mods.IndustrialExpansion.manager;
 
-/**
- * Created by hp on 11/01/2016.
- */
+import brazillianforgers.mods.IndustrialExpansion.helper.ConfigHandler;
+import brazillianforgers.mods.IndustrialExpansion.items.*;
+import cpw.mods.fml.common.event.FMLInterModComms;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import cofh.api.item.
+
 public class RecipeManager {
+    public static void init() {
+        addSmelterRecipe(ConfigHandler.energyCoalBall, new ItemStack(Items.coal), new ItemStack(Items.flint), MetaItemGetter.conductiveIron);
+        addSmelterRecipe(ConfigHandler.energyPhasedIron, new ItemStack(Items.iron_ingot), new ItemStack(Items.ender_pearl), MetaItemGetter.phasedIron);
+    }
+
+    public static void addSmelterRecipe(int paramInt, ItemStack paramItemStack1, ItemStack paramItemStack2, ItemStack paramItemStack3)
+    {
+        NBTTagCompound toSend = new NBTTagCompound();
+
+        toSend.setInteger("energy", paramInt);
+        toSend.setTag("primaryInput", new NBTTagCompound());
+        toSend.setTag("secondaryInput", new NBTTagCompound());
+        toSend.setTag("primaryOutput", new NBTTagCompound());
+
+        paramItemStack1.writeToNBT(toSend.getCompoundTag("primaryInput"));
+        paramItemStack2.writeToNBT(toSend.getCompoundTag("secondaryInput"));
+        paramItemStack3.writeToNBT(toSend.getCompoundTag("primaryOutput"));
+
+        FMLInterModComms.sendMessage("ThermalExpansion", "SmelterRecipe", toSend);
+    }
 }
