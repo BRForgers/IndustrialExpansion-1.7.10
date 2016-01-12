4a6eaeb4a0f7445394371d00800070af96b18c24
allanf181 <allanfarmelin2001@yahoo.com.br>
2016-01-11 23:45:26 -0200

    Agr tem que ir msm.

--- src/main/java/brazillianforgers/mods/IndustrialExpansion/proxy/CommonProxy.java
+++ src/main/java/brazillianforgers/mods/IndustrialExpansion/proxy/CommonProxy.java
@@ -2,6 +2,8 @@ package brazillianforgers.mods.IndustrialExpansion.proxy;
 
 import brazillianforgers.mods.IndustrialExpansion.manager.ItemManager;
 import brazillianforgers.mods.IndustrialExpansion.manager.MachineManager;
+import brazillianforgers.mods.IndustrialExpansion.manager.RecipeManager;
+import brazillianforgers.mods.IndustrialExpansion.manager.SmelterRecipeManager;
 
 public class CommonProxy
 {
@@ -9,6 +11,8 @@ public class CommonProxy
     public void preInit() {
         MachineManager.init();
         ItemManager.init();
+        RecipeManager.init();
+        SmelterRecipeManager.init();
     }
 
 }
